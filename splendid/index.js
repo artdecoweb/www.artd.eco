073c1652a6974a46a75eb4f575010104a7be5d0f
Anton <anton@adc.sh>
2019-05-15 15:22:17 +0300

    add splendid

---
+++ splendid/index.js
@@ -0,0 +1,21 @@
+import { join } from 'path'
+
+const APP_DIR = 'splendid'
+const BUILD_DIR = 'docs'
+
+/** @type {import('splendid').Config} */
+const config = {
+  appDir: APP_DIR,
+  layout: join(APP_DIR, 'layout/main.html'),
+  replacements: [
+    {
+      re: /{{ company }}/g,
+      replacement: '[Art Deco](https://artd.eco)',
+    },
+  ],
+  output: BUILD_DIR,
+  // to generate sitemaps:
+  url: 'https://artd.eco',
+}
+
+export default config
