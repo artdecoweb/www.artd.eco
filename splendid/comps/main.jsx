67bc2ea578d05e4d491636967e50f2e9f02320be
Anton <anton@adc.sh>
2019-05-17 18:08:21 +0300

    code background & birds

--- splendid/comps/main.jsx
+++ splendid/comps/main.jsx
@@ -13,14 +13,24 @@ import Components from '../components'
   key: 'parallax',
   id: 'c74268',
   props: {
+    'background-image': '/img/code2.gif',
+class: 'col-lg-5 align-self-stretch',
+style: 'background-repeat-x: no-repeat; margin-top:1.5rem; margin-bottom:1.5rem;padding:1.4rem;',
+  },
+  children: ["\n    <div class=\"d-flex justify-content-center\" style=\"height:100%;\">\n      <img alt=\"Node.JS at the epicenter of modern software development\" src=\"/img/Byss-und-Abyss.gif\" class=\"img-fluid align-self-center\" style=\"filter:drop-shadow(0 0 30px #00000047)\"/>\n    </div>\n  "],
+},
+{
+  key: 'parallax',
+  id: 'c80',
+  props: {
     'background-image': '/img/seamless.jpg',
-class: 'text-center col-lg-3 align-self-stretch mb-3 text-center',
+class: 'col-lg-3 align-self-stretch mb-3',
   },
   children: ["\n    <div class=\"d-flex justify-content-center\" style=\"height:100%;\">\n      <img src=\"img/images2.png\" class=\"img-fluid align-self-center\">\n    </div>\n  "],
 },
 {
   key: 'parallax',
-  id: 'c80',
+  id: 'c75046',
   props: {
     style: 'height:100px;',
 'background-image': '/img/bakst1.jpg',
