9abc7b814d1691926d04a7bb31500e7a8536d3f8
Anton <anton@adc.sh>
2019-05-21 01:56:33 +0300

    headings

--- splendid/components/Feature.jsx
+++ splendid/components/Feature.jsx
@@ -1,12 +1,17 @@
 const Feature = ({ children, title, direction = 'right', src, splendid, background, color, shadow }) => {
   splendid.renderAgain()
   const f = `float-${direction}`
+  const sf = `float-${direction == 'right' ? 'left' : 'right'}`
+  const st = direction == 'right' ? {} : {
+    'transform': 'scaleX(-1)',
+    '-webkit-transform': 'scaleX(-1)',
+  }
   const p = `m${direction == 'right' ? 'l' : 'r'}-3`
   const cn = ['img-fluid img-fluid rounded', f, p].join(' ')
   return (<row className="Feature">
     <div className="col">
+      <h2 className="text-center" style={{ background, color, 'text-shadow': shadow }}><img style={st} className={sf} src="img/sun.png" /> {title}</h2>
       <img alt={title} className={cn} src={src}/>
-      <h2 style={{ background, color, 'text-shadow': shadow }}><img src="img/sun.png"/> {title}</h2>
       <p>
         {children}
       </p>
