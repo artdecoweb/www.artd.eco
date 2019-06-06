c98fedd876a62d385d85a84d086fabeb075fd577
Anton <anton@adc.sh>
2019-06-06 11:12:22 +0300

    sky 1 + 2

--- splendid/components/Sky.jsx
+++ splendid/components/Sky.jsx
@@ -1,10 +1,11 @@
-const Sky = ({ count = 5 }) => {
+const Sky = ({ count = 5, splendid }) => {
   const arr = Array.from({ length: count })
   let i = 1
   return (<div className="Sky h-100 w-100 position-absolute" style="top:-50px;">
     {arr.map((j, n) => {
       const even = !!(n%2)
       const img = `/img/sky${even ? 1 : 2}.png`
+      splendid.addFile(img)
       // const img = '/img/layers.gif'
       const style = {
         top: n * 45,
