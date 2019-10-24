bb8fc8b7d5fb0dbd466c01c797e2e57ca5f9a0ed
Anton <anton@adc.sh>
2019-10-24 22:20:25 +0300

    start font preloading entry

---
+++ splendid/components/fig.jsx
@@ -0,0 +1,13 @@
+export default function Fig({ src, alt, splendid, border }) {
+  splendid.figures = splendid.figures || []
+  splendid.figures.push(alt)
+  const i = splendid.figures.length
+  const b = border ? { [`border-${border}`]: true } : {}
+  return (<bootstrap-div position-relative mb-3 overflow="hidden" p-3 rounded>
+    <parallax speedy="-0.5" z-index="-1" background-image="/img/letters/background.png" />
+    <p text-center>
+      <splendid-img border {...b} rounded img-fluid src={src} alt={alt} placeholder-auto />
+    </p>
+    <h5 m-0 background="white" d-inline-block>Figure {i}: {alt}</h5>
+  </bootstrap-div>)
+}
